d76ee7568e93d44b155f358d1c3e050b2a2685b8
Nicolas Andres <nicolasemilianoandres@hotmail.com>
2021-04-05 15:32:16 -0300

    tests

--- src/components/MembersList/__test__/MemberList.test.js
+++ src/components/MembersList/__test__/MemberList.test.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import { render, cleanup, fireEvent } from '@testing-library/react';
-import thunk from 'redux-thunk';
+import { render, cleanup } from '@testing-library/react'
+import thunk from 'redux-thunk'
 import { initialState } from '../../../reducers/membersReducer'
 import reducer from '../../../reducers/rootReducer'
 import MembersList from '../index'
@@ -11,21 +11,20 @@ const renderWithRedux = (
   component,
   { initialState, store = createStore(reducer, initialState, applyMiddleware(thunk)) } = {}
 ) => {
-    console.log('ESTADOOOOOOOOOOOOOOOOOOOOOOOOOO: ', initialState)
   return {
     ...render(<Provider store={store}>{component}</Provider>),
-    store,
+    store
   }
 }
 
- afterEach(cleanup);
+afterEach(cleanup)
 
 it('checks initial state is correct', () => {
-    const { getByTestId } = renderWithRedux(<MembersList />)
-    expect(getByTestId('memberList')).toHaveTextContent({
-        allMembers: [],
-        membersToDisplay: [],
-        loading: false,
-        error: false
-    })
+  const { getByTestId } = renderWithRedux(<MembersList />)
+  expect(getByTestId('memberList')).toHaveTextContent({
+    allMembers: [],
+    membersToDisplay: [],
+    loading: false,
+    error: false
   })
+})
